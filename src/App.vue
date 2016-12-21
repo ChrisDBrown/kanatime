29b905a9c918e92062198ce970720acf69cc2259
ChrisDBrown <owner@chrisdbrown.co.uk>
2016-12-21 17:01:49 +0000

    MVP! MVP! MVP!

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <h2>{{ randomKana.hiragana }}</h2>
+      <input type="text" name="romanji" v-on:keyup.enter="submitGuess(randomKana.romanji)">
+    </div>
+  </div>
+</template>
+
+<script>
+import baseData from './hiragana.json'
+
+export default {
+  name: 'app',
+  data () {
+    return  {
+      kana: baseData
+    }
+  },
+  computed: {
+    randomKana: function() {
+      return this.kana[Math.floor(Math.random()*this.kana.length)]
+    }
+  },
+  methods: {
+    submitGuess: function(correct) {
+      if (correct == event.target.value) {
+        alert('Correct!');
+      } else {
+        alert('Incorrect');
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

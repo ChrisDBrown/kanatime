d0a2f9520bfdba09cfa6a93278f6e5c0a69f680f
ChrisDBrown <owner@chrisdbrown.co.uk>
2016-12-22 12:31:22 +0000

    Add eslinting & scoped CSS extraction

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="card">
       <h2>{{ randomKana.hiragana }}</h2>
-      <input type="text" name="romanji" v-on:keyup.enter="submitGuess(randomKana.romanji)">
+      <input type="text" name="romanji" v-on:keyup.enter="submitGuess(randomKana.romanji, $event)">
     </div>
   </div>
 </template>
@@ -13,16 +13,16 @@ import kanaMap from './hiragana.json'
 export default {
   name: 'app',
   computed: {
-    randomKana: function() {
+    randomKana: function () {
       return kanaMap[Math.floor(Math.random() * kanaMap.length)]
     }
   },
   methods: {
-    submitGuess: function(correct) {
-      if (correct == event.target.value) {
-        alert('Correct!');
+    submitGuess: function (correct, $event) {
+      if (correct === $event.target.value) {
+        window.alert('Correct!')
       } else {
-        alert('Incorrect');
+        window.alert('Incorrect')
       }
     }
   }

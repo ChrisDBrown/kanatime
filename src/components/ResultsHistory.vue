1719fb633c99b22bc07ac5133445ea0407bcacb7
ChrisDBrown <owner@chrisdbrown.co.uk>
2016-12-31 14:27:10 +0000

    Add results history component

---
+++ src/components/ResultsHistory.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="history">
+    <table style="background-color: white;">
+      <tr>
+        <th>Kana</th>
+        <th>Romanji</th>
+        <th>Success</th>
+        <th>Failure</th>
+        <th>Skipped</th>
+      </tr>
+      <tr v-for="kana in resultsHistory">
+        <td>{{ kana.kana }}</td>
+        <td>{{ kana.romanji }}</td>
+        <td>{{ kana.results.success }}</td>
+        <td>{{ kana.results.failure }}</td>
+        <td>{{ kana.results.skipped }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters(['resultsHistory'])
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -13,3 +13,12 @@ export const hasAnswered = state => {
 export const answeredCorrectly = state => {
   return state.volatile.answeredCorrectly
 }
+
+export const resultsHistory = state => {
+  const history = state.kanadb.map(function (kana) {
+    const matchingResults = this.find(results => results.id === kana.id)
+    kana.results = matchingResults
+    return kana
+  }, state.results)
+  return history
+}
